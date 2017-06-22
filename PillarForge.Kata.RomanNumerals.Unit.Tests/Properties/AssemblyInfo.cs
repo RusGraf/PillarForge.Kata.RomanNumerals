05b950a85c629cd904700ff33633108e7c567eb4
RusGraf <rguryanov@gmail.com>
2017-06-22 13:08:57 -0400

    Initial Creation of the RomanNumerals Kata

---
+++ PillarForge.Kata.RomanNumerals.Unit.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PillarForge.Kata.RomanNumerals.Unit.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OEConnection")]
+[assembly: AssemblyProduct("PillarForge.Kata.RomanNumerals.Unit.Tests")]
+[assembly: AssemblyCopyright("Copyright © OEConnection 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da02ca0e-0921-450a-b2b3-6344470286e8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
